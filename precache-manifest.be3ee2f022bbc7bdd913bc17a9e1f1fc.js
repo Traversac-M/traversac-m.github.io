ffdce37788de7338cd4d266b499b821f3e807048
Michel <michel.traversac@epitech.eu>
2019-03-17 19:56:37 +0100

    Updates

---
+++ precache-manifest.be3ee2f022bbc7bdd913bc17a9e1f1fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "74bf54342ff1fdace863",
+    "url": "/static/js/main.04f20c04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "6a651558f27afd1cb06283ec3221ffea",
+    "url": "/index.html"
+  }
+];
